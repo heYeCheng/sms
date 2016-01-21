3ad981db44ea03064d292c70871f5b5eb06a1bbd
HeYecheng <fshyhyc@gmail.com>
2016-01-21 10:11:58 +0800

    f

--- testsms.py
+++ testsms.py
@@ -23,28 +23,30 @@ import serial.tools.list_ports
 
 class SMS(object):
 	"""docstring for SMS"""
-	def __init__(self, port, bit):
+	def __init__(self):
 		# self.arg = arg
-		self.start(port, bit)
+		print 'test'
 
 	def start(self, port, bit):
+		print 'start'
 		'启动端口'
 		ser = serial.Serial(port, bit)
-		ser.write("AT+CMGF=1/r")
-
-		pdus4=""
-		loop_time = 0
-		while(pdus4.find('OK/r/n')<0):
-			mydata4=ser.read(65536)
-			pdus4=pdus4+mydata4
-			time.sleep(0.1)
-			if loop_time > 5:
-				break
-			else:
-				loop_time += 1
-
 		print ser
-		print pdus4
+		# ser.write("AT+CMGF=1/r")
+
+		# pdus4=""
+		# loop_time = 0
+		# while(pdus4.find('OK/r/n')<0):
+		# 	mydata4=ser.read(65536)
+		# 	pdus4=pdus4+mydata4
+		# 	time.sleep(0.1)
+		# 	if loop_time > 5:
+		# 		break
+		# 	else:
+		# 		loop_time += 1
+
+		# print ser
+		# print pdus4
 
 	def send(self, cont):
 		#start send message'已收到短信');
@@ -56,4 +58,6 @@ class SMS(object):
 		ser.write("AT+CMGS=47/r");
 
 
-sms = SMS('com3', 115200)
+sms = SMS()
+sms.start('com3', 115200)
+
